c29960d8ccb2708ed7345b15571bb2102f22befc
moskovsky <whatcodingrose@gmail.com>
2024-03-16 15:45:30 +0300

    6

--- test_company_service.py
+++ test_company_service.py
@@ -2,18 +2,15 @@ import requests
 
 def test_get_all_companies(url: str):
     res = requests.get(url).json()
-    assert(res == [{'employees_id': 1, 'name': 'Альфа-Банк',
+    assert(res == [{'companies_id': 1, 'name': 'Альфа-Банк',
      'descrption': 'Крупнейшее финансово-кредитное учреждение с универсальным подходом к ведению бизнеса',
      'industry': 'Economy', 'age': '34'},
-    {'employees_id': 2, 'name': 'Ozon',
+    {'companies_id': 2, 'name': 'Ozon',
      'descrption': 'Ведущая мультикатегорийная платформа электронной коммерции и одна из крупнейших интернет-компаний в России',
      'industry': 'MarketPlace', 'age': '26'},
-    {'employees_id': 3, 'name': 'Apple',
+    {'companies_id': 3, 'name': 'Apple',
      'descrption': 'Американская корпорация, разработчик персональных и планшетных компьютеров, аудиоплееров, смартфонов, программного обеспечения и цифрового контента',
      'industry': 'Electronic Device', 'age': '47'}])
-
-
-
 
 
 def test_get_company_by_id(url: str):
